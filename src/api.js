a238be6ba17f4686b6ea7455cc1449f01e6be631
PiFlea Dev <dev@piflea.local>
2026-07-22 13:42:03 +0000

    debug: log accessToken info in apiFetch

--- src/api.js
+++ src/api.js
@@ -14,6 +14,11 @@ export async function apiFetch(path, options = {}) {
   const headers = { 'Content-Type': 'application/json', ...options.headers };
   if (user && user.accessToken) {
     headers['Authorization'] = 'Bearer ' + user.accessToken;
+    console.log('[DEBUG apiFetch] accessToken exists:', !!user.accessToken, 'length:', user.accessToken.length);
+    console.log('[DEBUG apiFetch] Authorization header added:', !!headers['Authorization']);
+  } else {
+    console.log('[DEBUG apiFetch] accessToken not found');
+    console.log('[DEBUG apiFetch] user:', user ? 'exists' : 'null');
   }
   const res = await fetch(BACKEND_URL + path, { ...options, headers });
   return res;
